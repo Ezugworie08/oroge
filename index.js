45d5e719b00d2312c1f3acd4e33956c0023c35d7
Ike Ezugworie <Ezugworie08@users.noreply.github.com>
2023-08-27 12:52:37 -0500

    Added an endpoint to recursively list files in the given log directory (#2)

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const express = require('express');
+
+const { listFiles } = require('./utils/index');
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+const rootDir = process.env.ROOT_DIR || './logs';
+const logDir = path.resolve(__dirname, rootDir);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the Oroge API!'});
+// This endpoint will return all the log files in the provided logs directory
+app.get('/', async (req, res) => {
+
+    try {
+        const logFiles = await listFiles(logDir);
+        console.log(logFiles);
+        res.json({logFiles});
+    } catch (err){
+        res.status(500).json({error: err});
+    }
+  
 });
+
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
