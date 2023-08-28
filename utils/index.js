743842712090b4573134c25f412e7567935a1413
Ike Ezugworie <Ezugworie08@users.noreply.github.com>
2023-08-28 03:40:55 -0500

    Feature: File streaming (#3)
    * Added the file streaming endpoint with search and limit query params
    
    * added default values for searchQuery and limit query params

--- utils/index.js
+++ utils/index.js
@@ -1,32 +1,33 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
+const readline = require('readline');
+const url = require('url');
 
-const listFiles = async (rootDir) => {
+
+const traverseDir = async (rootDir) => {
     const results = [];
 
     async function walk(currentDir) {
         try {
-            const files = await fs.readdir(currentDir);
-
-            console.log(`Found ${files.length} files in directory ${currentDir}`);
+            const files = await fsp.readdir(currentDir);
 
             for (const file of files) {
                 const filePath = path.join(currentDir, file);
-                const stats = await fs.stat(filePath);
+                const stats = await fsp.stat(filePath);
 
                 if (stats.isDirectory()) {
                     await walk(filePath); // Recurse into subdirectories
                 } else if (stats.isFile()) {
-                    const { base, dir } = path.parse(filePath);
-                    const baseDir = dir.split(rootDir).pop();
-                    const qFilePath = path.join(baseDir, base);
-                    results.push(qFilePath);
+                    const fileURL = new url.URL(`file://${filePath}`);
+                    const encodedFilePath = encodeURIComponent(filePath);
+                    results.push({filePath, fileURL, encodedFilePath});
                 }
             }
 
         } catch (err) {
-            console.log("Error walking the logs directory:", err);
-            return err.message;
+            console.error("Error walking the provided directory:", err);
+            throw new Error(err.message || "Error walking the provided directory");
         }
     };
 
@@ -34,11 +35,46 @@ const listFiles = async (rootDir) => {
         await walk(rootDir);
         return results;
     } catch (err) {
-        console.log("Error reading log files:", err);
-        return err.message;
+        console.error("Error listing files in provided directory:", err);
+        throw new Error(err.message || "Error listing files in provided directory");
     }
 };
 
+const readLastNLines = async ({filePath, nLines, searchQuery}) => {
+    const results = [];
+    const isValidSearchQuery = !!searchQuery && typeof searchQuery !== 'undefined' && searchQuery.length > 0;
+
+    try {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(filePath), 
+            crlfDelay: Infinity
+        });
+
+        for await (const line of rl) {
+    
+            if (!isValidSearchQuery) {
+                results.push(line);
+            } else {
+                const foundSearchString = String(line).includes(searchQuery);
+                if (foundSearchString) {
+                    results.push(line);
+                }
+            }
+
+            if (results.length > nLines) {
+                results.shift();
+            }
+        }
+
+        return results;
+    } catch (err) {
+        console.error("Error reading log file:", err);
+        throw new Error(err.message || "Error reading log file");
+    }
+};
+
+
 module.exports = {
-    listFiles
+    traverseDir,
+    readLastNLines,
 };
