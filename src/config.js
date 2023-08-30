6b443b348a6b5fc2b2097fb2d8e12c3d48b4ca9d
Ike Ezugworie <Ezugworie08@users.noreply.github.com>
2023-08-30 13:34:43 -0500

    quick fix for windows, updated license (#15)

--- src/config.js
+++ src/config.js
@@ -3,9 +3,12 @@ const path = require('path');
 const { hasLogDirReadAccess } = require('./utils');
 const { WINDOWS_ROOT_DIR, UNIX_ROOT_DIR, TEST_ROOT_DIR } = require('./utils/constants');
 
-const ROOT_DIR = process.platform === 'win32' ? path.resolve(__dirname, WINDOWS_ROOT_DIR) : path.resolve(__dirname, UNIX_ROOT_DIR)
-const logDir = hasLogDirReadAccess(ROOT_DIR) ? ROOT_DIR : path.resolve(__dirname, TEST_ROOT_DIR)
+const ROOT_DIR = process.platform === 'win32' ? path.resolve(__dirname, WINDOWS_ROOT_DIR) : path.resolve(__dirname, UNIX_ROOT_DIR);
+
+const testDir = path.resolve(__dirname, TEST_ROOT_DIR);
+const logDir = hasLogDirReadAccess(ROOT_DIR) ? ROOT_DIR : testDir; 
 
 module.exports = {
     logDir,
+    testDir,
 };
